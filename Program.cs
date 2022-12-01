dd6a4581077f677a5cc51d0424f41571da23678c
KonstantinVasiliev <grafit21@mail.ru>
2022-12-01 22:34:13 +0500

    save metod FullAr

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[n];
+
+void FullAr(string[] arr)
+{
+    int k=0;
+    for(int i = 0; i < n; i++)
+    {
+        k++;
+        Console.Write($"Введите {k} эллемент массива: ");
+        Console.ReadLine();
+    }
+}
+
+FullAr(array);
